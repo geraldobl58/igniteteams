f863e0420e021d31e4d18a00f6a626507726f154
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-02 17:12:55 -0300

    feat: Add loading component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,3 @@
-import { ActivityIndicator } from 'react-native';
-
 import { ThemeProvider } from 'styled-components';
 
 import {
@@ -9,6 +7,7 @@ import {
 } from '@expo-google-fonts/roboto'
 
 import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
 
 import theme from '@theme/index';
 
@@ -17,7 +16,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
