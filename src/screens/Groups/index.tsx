eec91ae7e115d33065ec965fedb1f2173bcee08b
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-02 16:44:13 -0300

    feat: Add theme and type definition file

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,9 @@
-import { Text } from "react-native";
-
 import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
-      <Text>Groups</Text>
+      
     </Container>
   )
 }
