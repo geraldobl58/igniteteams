b5c68b6d035d8ce0f22f1e69fa0403d531633832
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-05 18:18:05 -0300

    feat: Add group card component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,4 @@
+import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
@@ -10,6 +11,10 @@ export function Groups() {
       <Highlight 
         title="Turmas"
         subtitle="jogue com sua turma"
+      />
+
+      <GroupCard 
+        title="Grupo A"
       />
     </Container>
   )
