b3cf78404376f81f6400ede6a6fef6583bcea6a6
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-14 17:26:50 -0300

    feat: Add list empty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,11 +5,12 @@ import { FlatList } from 'react-native'
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
 
 import { Container } from "./styles";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Grupo A', 'Grupo B'])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -27,6 +28,8 @@ export function Groups() {
             title={item} 
           />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma" />}
       />
 
     </Container>
