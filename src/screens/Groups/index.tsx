d4b3979ef292f0eeee088c6863b338dc4a989922
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-15 19:29:06 -0300

    feat: Adds focus to the screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { FlatList } from 'react-native'
 
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+
+import { groupGetAll } from "@storage/group/groupGetAll";
 
 import { Container } from "./styles";
 
@@ -20,6 +22,19 @@ export function Groups() {
   function handleNewGroup() {
     navigation.navigate('new')
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupGetAll()
+      setGroups(data)
+    }catch(error) {
+      console.log(error)
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups()
+  }, []))
 
   return (
     <Container>
