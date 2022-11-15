8c33b574be952997cda76f5ffc11ea3f1e84192b
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-15 15:36:54 -0300

    feat: Add listing filters

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
@@ -17,4 +17,21 @@ export const Form = styled.View`
   border-radius: 6px;
 
   background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+`
+
+export const HeaderList = styled.View`
+  width: 100%;
+
+  align-items: center;
+  flex-direction: row;
+
+  margin: 32px 0 12px;
+`
+
+export const NumberOfPlayers = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-size: ${theme.FONT_SIZE.SM}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `}
 `
