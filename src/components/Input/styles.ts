7d33ad8c59feb949c91afed5c56461ea21c03de6
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-15 13:29:42 -0300

    feat: Add input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,20 @@
+import { TextInput } from "react-native";
+
+import styled from "styled-components/native";
+
+export const Container = styled(TextInput)`
+  flex: 1;
+
+  min-height: 56px;
+  max-height: 56px;
+
+  padding: 16px;
+
+  border-radius: 6px;
+
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+`
